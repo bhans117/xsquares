91f0c501ccb912d8c7a05d58986dcef8d4994707
Benjamin Hansen <brhansen3@wisc.edu>
2017-10-06 19:22:44 -0500

    add phaser and phaser tutorial

---
+++ game.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+	var canvasWidth = 832, canvasHeight = 640;
+	var game = new Phaser.Game(canvasWidth, canvasHeight, Phaser.AUTO, '', {preload: preload, create: create, update: update });
+	function preload() {}
+	function create() {}
+	function update() {}
+};
